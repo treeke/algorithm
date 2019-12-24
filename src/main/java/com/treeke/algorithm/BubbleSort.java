75d65afea709c4267f9d0da3862336af9dd0cf51
weiye <weiye@cetcxl.com>
2019-12-24 10:19:21 +0800

    add

---
+++ src/main/java/com/treeke/algorithm/BubbleSort.java
@@ -0,0 +1,33 @@
+package com.treeke.algorithm;
+
+/**
+ * 冒泡排序
+ * @author treeke
+ *
+ */
+public class BubbleSort {
+
+	public static void main(String[] args) {        
+	    int arr[] = new int[]{3,3,3,7,9,122344,4656,34,34,5,6,4656,7,8,9,343,57765,23,12321};        
+	    int len = arr.length-1;      
+	    long start = System.currentTimeMillis();  
+	    arr = sort(arr);
+	    System.out.println(System.currentTimeMillis() - start);
+	    for (int i:arr) {            
+	        System.out.print(i + " ");        
+	    }    
+	}
+	
+	public static int[] sort(int [] arr) {
+		for(int i = 0; i < arr.length; i++) {
+			for(int j = i; j < arr.length; j++) {
+				if(arr[i] > arr[j]) {
+					arr[i] = arr[j] + arr[i];
+					arr[j] = arr[i] - arr[j];
+					arr[i] = arr[i] - arr[j];
+				} 
+			}
+		}
+		return arr;
+	}
+}
--- src/main/java/com/treeke/algorithm/QuickSort.java
+++ src/main/java/com/treeke/algorithm/QuickSort.java
@@ -1,12 +1,17 @@
 package com.treeke.algorithm;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 /**
  * 快速排序
  * @author treeke
  *
  */
 public class QuickSort {
-	public static int[] qsort(int arr[],int start,int end) {        
+	public static AtomicInteger q = new AtomicInteger();
+	
+	public static int[] qsort(int arr[],int start,int end) { 
+		System.out.println(q.incrementAndGet() + "-" + start + "-" + end);
 	    int pivot = arr[start];        
 	    int i = start;        
 	    int j = end;        
@@ -27,16 +32,19 @@ public class QuickSort {
 	    }        
 	    if (i-1>start) arr=qsort(arr,start,i-1);        
 	    if (j+1<end) arr=qsort(arr,j+1,end);        
-	    return (arr);    
+	    return arr;    
 	}    
 	 
 	public static void main(String[] args) {        
-	    int arr[] = new int[]{3,3,3,7,9,122344,4656,34,34,4656,5,6,7,8,9,343,57765,23,12321};        
-	    int len = arr.length-1;        
-	    arr=qsort(arr,0,len);        
+	    int arr[] = new int[]{3,3,3,7,9,122344,4656,34,34,5,6,4656,7,8,9,343,57765,23,12321};        
+	    int len = arr.length-1;      
+	    long start = System.currentTimeMillis();
+	    arr=qsort(arr,0,len);    
+	    System.out.println(System.currentTimeMillis() - start);
 	    for (int i:arr) {            
 	        System.out.print(i + " ");        
 	    }    
 	}
+	
 
 }
