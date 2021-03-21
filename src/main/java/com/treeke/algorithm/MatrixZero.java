9b195b53339fdfe96d29a8d0816995a77d9d6458
treeke <pickyourself@163.com>
2021-03-21 13:57:55 +0800

    2021-3-21

---
+++ src/main/java/com/treeke/algorithm/MatrixZero.java
@@ -0,0 +1,42 @@
+package com.treeke.algorithm;
+
+/**
+ * 给定一个 m x n 的矩阵，如果一个元素为 0 ，则将其所在行和列的所有元素都设为 0 。请使用 原地 算法。
+ *
+ * 进阶：
+ *
+ * 一个直观的解决方案是使用  O(mn) 的额外空间，但这并不是一个好的解决方案。
+ * 一个简单的改进方案是使用 O(m + n) 的额外空间，但这仍然不是最好的解决方案。
+ * 你能想出一个仅使用常量空间的解决方案吗？
+ */
+public class MatrixZero {
+
+    public static void main(String[] args) {
+        //循环存储置零信息到最左和最上
+        //倒序判断当前位置的最左和最上是否为0  赋值当前位置为0
+        //单独判断第一行第一个是否为0
+    }
+
+    public void setZeroes(int[][] matrix) {
+        boolean col0_flag = false;
+        int row = matrix.length;
+        int col = matrix[0].length;
+        for (int i = 0; i < row; i++) {
+            if (matrix[i][0] == 0) col0_flag = true;
+            for (int j = 1; j < col; j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[i][0] = matrix[0][j] = 0;
+                }
+            }
+        }
+        for (int i = row - 1; i >= 0; i--) {
+            for (int j = col - 1; j >= 1; j--) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+                    matrix[i][j] = 0;
+                }
+            }
+            if (col0_flag) matrix[i][0] = 0;
+        }
+
+    }
+}
