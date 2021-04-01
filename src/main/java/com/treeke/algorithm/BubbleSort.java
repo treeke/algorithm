7d45df64fdaebc13ad9c978e81ea5e5b9a388fc4
treeke <pickyourself@163.com>
2021-04-02 00:23:05 +0800

    2021-4-2

--- src/main/java/com/treeke/algorithm/BubbleSort.java
+++ src/main/java/com/treeke/algorithm/BubbleSort.java
@@ -20,7 +20,7 @@ public class BubbleSort {
 	
 	public static int[] sort(int [] arr) {
 		for(int i = 0; i < arr.length; i++) {
-			for(int j = i; j < arr.length; j++) {
+			for(int j = i+1; j < arr.length; j++) {
 				if(arr[i] > arr[j]) {
 					arr[i] = arr[j] + arr[i];
 					arr[j] = arr[i] - arr[j];
--- src/main/java/com/treeke/algorithm/QuickSort.java
+++ src/main/java/com/treeke/algorithm/QuickSort.java
@@ -36,15 +36,37 @@ public class QuickSort {
 	}    
 	 
 	public static void main(String[] args) {        
-	    int arr[] = new int[]{3,3,3,7,9,122344,4656,34,34,5,6,4656,7,8,9,343,57765,23,12321};        
-	    int len = arr.length-1;      
+	    //int arr[] = new int[]{3,3,3,7,9,122344,4656,34,34,5,6,4656,7,8,9,343,57765,23,12321};
+	    int arr[] = new int[]{72,6,57,88,60,42,83,73,48,85};
+	    int len = arr.length-1;
 	    long start = System.currentTimeMillis();
-	    arr=qsort(arr,0,len);    
+	    arr=quick_sort(arr,0,len);
 	    System.out.println(System.currentTimeMillis() - start);
 	    for (int i:arr) {            
 	        System.out.print(i + " ");        
 	    }    
 	}
+
+    //快速排序
+    public static int[] quick_sort(int[] s, int l, int r) {
+        if (l < r) {
+            //Swap(s[l], s[(l + r) / 2]); //将中间的这个数和第一个数交换 参见注1
+            int i = l, j = r, x = s[l];
+            while (i < j) {
+                // 从右向左找第一个小于x的数
+                while(i < j && s[j] >= x) j--;
+                if(i < j) s[i++] = s[j];
+                // 从左向右找第一个大于等于x的数
+                while(i < j && s[i] < x) i++;
+                if(i < j) s[j--] = s[i];
+            }
+            s[i] = x;
+            // 递归调用
+            quick_sort(s, l, i - 1);
+            quick_sort(s, i + 1, r);
+        }
+        return s;
+    }
 	
 
 }
